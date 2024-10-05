8e17de4b4fc165dcc3704233e148e9197eed4aef
Your Name <227001221 @itsx.edu.mx>
2024-10-05 13:20:05 -0600

    Navegation Service

--- Front/MainWindow.xaml.cs
+++ Front/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Front.Core.Services.Navigation;
+using Microsoft.Extensions.DependencyInjection;
+using Wpf.Ui.Controls;
 
 namespace Front;
 
@@ -16,6 +19,8 @@ namespace Front;
 /// </summary>
 public partial class MainWindow
 {
+    private INavigationService _navigationService = App.Current.Services.GetService<INavigationService>()!; 
+
     public MainWindow()
     {
         InitializeComponent();
