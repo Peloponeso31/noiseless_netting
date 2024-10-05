23ca7e2097232f0d2457fe3ba6bc933ecf325dd7
Peloponeso31 <tanilizquierdo@proton.me>
2024-10-05 16:01:16 -0600

    Se agrega ListaEventosViewModel.cs

---
+++ Front/Features/ListaEventos/ListaEventosViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Front.Features.ListaEventos;
+
+public partial class ListaEventosViewModel : ObservableObject
+{
+    [ObservableProperty] private ObservableCollection<string> _miniSeeds;
+
+    public ListaEventosViewModel()
+    {
+        var myDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        var files = Directory.GetFiles(Path.Combine(myDocumentsPath, "MiniSeeds"));
+
+        MiniSeeds = new ObservableCollection<string>(files);
+    }
+}
