735e64fb60ac71b0c1bf319b50c513553a54f413
Jonatan Luna Franco <157180405+untaljnn@users.noreply.github.com>
2024-10-05 12:43:41 -0600

    create backend with basic argvs

--- Front/Backend/backend.py
+++ Front/Backend/backend.py
@@ -1,5 +1,24 @@
-﻿def main():
-    print('Goodbye, world!')
+﻿import argparse
+from obspy.core import read
+
+# Parser initializer
+parser = argparse.ArgumentParser(
+    prog='Noiseless netting',
+    description='Filtering noisy data',
+    epilog='AxoloTech')
+
+# Valid arguments
+parser.add_argument('--filename', required=True)
+parser.add_argument('--graphic', choices=['plot', 'spectrogram'])
+args = parser.parse_args()
+
+single_channel = read(args.filename)
+single_channel.plot()
+
+
+def main():
+    print(args.filename)
+
 
 if __name__ == '__main__':
     main()
